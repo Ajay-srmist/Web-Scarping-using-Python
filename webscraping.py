cf3d8f44441db4481344e88118878bf3602d7ea5
Ajay Bhaskar H <104590091+Ajay-srmist@users.noreply.github.com>
2024-10-11 15:48:08 +0530

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,82 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
 
-url="https://getpython.wordpress.com/"
-BASE_URL = "https://getpython.wordpress.com/"
-source=requests.get(url)
+def scrape_imdb_top_250():
+    try:
+        # Set the user-agent to mimic a browser request
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+        
+        # Fetch the IMDb Top 250 page
+        response = requests.get('https://www.imdb.com/chart/top/', headers=headers)
+        response.raise_for_status()  # Check if the request was successful
+        
+        # Parse the page content using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Print the HTML content to check if we're getting the correct page
+        print("Page fetched successfully. Checking HTML structure...")
+        print(soup.prettify()[:1000])  # Print only the first 1000 characters of the page for inspection
 
-def get_chrome_web_driver(options):
-    return webdriver.Chrome("./chromedriver", chrome_options=options)
+        # Find the table containing the top movies
+        movies_table = soup.select('table.chart.full-width tr')
+        print(f"Found {len(movies_table)} rows in the table.")  # Debugging statement
 
-def get_web_driver_options():
-    return webdriver.ChromeOptions()
+        # Check if any rows are found
+        if len(movies_table) > 0:
+            top_movies = []
 
-def set_ignore_certificate_error(options):
-    options.add_argument('--ignore-certificate-errors')
+            # Loop through each movie entry (skipping the header row)
+            for movie in movies_table[1:]:
+                title_column = movie.find('td', class_='titleColumn')
+                if title_column:
+                    rank = title_column.get_text(strip=True).split('.')[0]  # Rank
+                    title = title_column.a.get_text()  # Title
+                    year = title_column.span.get_text(strip=True).strip('()')  # Release year
+                    rating_column = movie.find('td', class_='ratingColumn imdbRating')
+                    rating = rating_column.strong.get_text() if rating_column else 'N/A'  # IMDb Rating
 
-def set_browser_as_incognito(options):
-    options.add_argument('--incognito')
+                    # Append movie data to the list
+                    top_movies.append({
+                        'rank': rank,
+                        'title': title,
+                        'year': year,
+                        'rating': rating
+                    })
+            
+            # Print the result
+            for movie in top_movies:
+                print(f"{movie['rank']}. {movie['title']} ({movie['year']}) - Rating: {movie['rating']}")
+        else:
+            print("No movies found in the table. Check the structure.")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-soup = BeautifulSoup(source.text, 'html')
+# Run the function to scrape the data
+scrape_imdb_top_250()
 
-title = soup.find('title')
-print("this is with html tags:", title)
-
-qwery = soup.find('h1')
-print("this is without html tags:", qwery.text)
-
-links = soup.find('a')
-print(links)
-
-print(links['href'])
-
-for a in soup.find_all('a', href=True):
-    print(a['href'])
-
-for i in links:
-    print(i.text)
-
-print(links['class'])
-
-many_link = soup.find_all('a')
-total_links = len(many_link)
-print("total links in my website:", total_links)
-print()
-for i in many_link[:6]:
-    print(i)
-
-second_link = many_link[1]
-print(second_link)
-print()
-print("href is:", second_link['href'])
-
-nested_div = second_link.find('div')
-print(nested_div)
-print()
-
-z = (nested_div['class'])
-print(z)
-print(type(z))
-print()
-
-print("class name of div is:", " ".join(nested_div['class']))
-
-wiki = requests.get("https://en.wikipedia.org/wiki/World_War_II")
-soup = BeautifulSoup(wiki.text, 'html')
-print(soup.find('title'))
-
-ww2_contents = soup.find_all("div", class_='toc')
-for i in ww2_contents:
-    print(i.text)
-
-overview = soup.find_all('table', class_='infobox vevent')
-for z in overview:
-    print(z.text)
-
-images = soup.find_all('img')
-
-print(images)
